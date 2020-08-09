32189da0f15ebbc0a5832e5c363dd659e7e1746e
GUYOT Julie <guyotjulie@MacBook-Pro-de-GUYOT.local>
2020-08-09 15:25:18 +0200

    add pagination

--- routes/characters.js
+++ routes/characters.js
@@ -6,8 +6,7 @@ const axios = require("axios");
 router.get("/characters", async (req, res) => {
   try {
     let limit = 100;
-    // let page = req.query.page;
-    // let offset = 100;
+    let offset = req.query.offset;
     const date = new Date();
 
     const timestamp = date.getTime() / 1000;
@@ -22,12 +21,12 @@ router.get("/characters", async (req, res) => {
 
     if (req.query.nameStartsWith) {
       const response = await axios.get(
-        `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${apikey}&hash=${hash}&limit=${limit}&orderBy=name&nameStartsWith=${req.query.nameStartsWith}`
+        `http://gateway.marvel.com/v1/public/characters?offset=${offset}&ts=${ts}&apikey=${apikey}&hash=${hash}&limit=${limit}&orderBy=name&nameStartsWith=${req.query.nameStartsWith}`
       );
       res.json(response.data);
     } else {
       const response = await axios.get(
-        `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${apikey}&hash=${hash}&limit=${limit}&orderBy=name`
+        `http://gateway.marvel.com/v1/public/characters?offset=${offset}&ts=${ts}&apikey=${apikey}&hash=${hash}&limit=${limit}&orderBy=name`
       );
       res.json(response.data);
     }
--- routes/charactersId.js
+++ routes/charactersId.js
@@ -6,6 +6,7 @@ const axios = require("axios");
 router.get("/characters/:id/comics", async (req, res) => {
   try {
     const date = new Date();
+    let offset = req.query.offset;
     const timestamp = date.getTime() / 1000;
     const ts = Math.floor(timestamp);
     const hash = md5(
@@ -17,7 +18,7 @@ router.get("/characters/:id/comics", async (req, res) => {
     const apikey = process.env.MARVEL_PUBLIC_API_KEY;
 
     const response = await axios.get(
-      `https://gateway.marvel.com/v1/public/characters/${req.params.id}/comics?ts=${ts}&apikey=${apikey}&hash=${hash}`
+      `https://gateway.marvel.com/v1/public/characters/${req.params.id}/comics?offset=${offset}&ts=${ts}&apikey=${apikey}&hash=${hash}`
     );
     res.json(response.data);
   } catch (error) {
